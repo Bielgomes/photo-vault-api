a2596694c122bbea6a85b9616c736d0afc78e41c
Bielgomes <bielgomesdasilva@hotmail.com>
2025-08-15 23:38:05 -0300

    fix: e2e tests not passing

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,7 @@
+import * as schema from 'drizzle/schema'
 import { reset } from 'drizzle-seed'
 import { app } from '@/app'
 import { db } from '@/lib/drizzle'
-import * as schema from '../../drizzle/schema'
 
 beforeAll(async () => {
   await app.ready()
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,14 @@ export default defineConfig({
   test: {
     setupFiles: ['./src/test/setup.ts'],
     globals: true,
-    root: './',
+    reporters: 'verbose',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'text-summary', 'html'],
+      all: true,
+      include: ['src/**/*.ts'],
+      exclude: ['**/*.test.ts', 'src/tests/**'],
+    },
   },
 })
